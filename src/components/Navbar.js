bd64ea10f4d875eaa7c2788e1e573ce43c199fc1
DP <data.rotor@rotormediagroup.se>
2022-01-24 13:57:33 +0100

    change

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -35,14 +35,14 @@ const Navbar = class extends React.Component {
   render() {
     return (
       <nav
-        className="navbar is-transparent"
+        className="navbar is-fixed-top is-spaced"
         role="navigation"
         aria-label="main-navigation"
       >
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: "88px" }} />
+              <img src={logo} alt="Rotor" style={{ width: "88px" }} />
             </Link>
             {/* Hamburger menu */}
             <div
